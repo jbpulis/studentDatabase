2061f9818eeb4dc52a4ca9e114806fedff3f8241
Jason <jbpulis@yahoo.com>
2019-08-16 15:44:12 -0600

    set up student
    add ability to enter first last and year

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,7 +3,9 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
+		Student stu1 = new Student();
 		// Ask how many users we want to add
+		
 		
 		// Create n number of new students
 
